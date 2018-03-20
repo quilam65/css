9d402a3df3a8100e7d02e85bbbd1278f2cd4f7a2
quilam65 <quilt@atmarkcafe.org>
2018-03-20 16:45:41 +0700

    first

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require bootstrap
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "100px";
+    document.getElementById("main").style.marginLeft= "100px";
+    // document.getElementById("mySidenav").style.padding = "8px 8px 8px 0";
+    $('#mySidenav').find('a').css('padding' : '8px 8px 8px 0');
+ }
